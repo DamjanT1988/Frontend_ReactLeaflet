2ffb5dc716d690c48476a54fac2db4aecb105a43
DamjanT1988 <Damjan_t@hotmail.com>
2023-12-28 11:16:46 +0100

    231228-1115

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polygon, Polyline, LayersControl, FeatureGroup, GeoJSON } from 'react-leaflet';
 import { EditControl } from "react-leaflet-draw";
 import L from 'leaflet';
@@ -10,7 +10,8 @@ const { BaseLayer } = LayersControl;
 
 const MyMap = () => {
     const [geoJsonData, setGeoJsonData] = useState(null); // State to hold GeoJSON data
-    const [featureGroupRef, setFeatureGroupRef] = useState(null); // Ref for the feature group    
+    //const [featureGroupRef, setFeatureGroupRef] = useState(null); // Ref for the feature group 
+    const featureGroupRef = useRef(null); // Use useRef for featureGroup   
     const [markers, setMarkers] = useState([]); // Array of marker positions
     const [lines, setLines] = useState([]); // Array of arrays of polyline positions
     const [polygons, setPolygons] = useState([]); // Array of arrays of polygon positions
@@ -20,20 +21,27 @@ const MyMap = () => {
 
     const updateGeoJson = () => {
         if (featureGroupRef.current) {
-            const featureGroup = featureGroupRef.current.leafletElement; // Get the Leaflet FeatureGroup
+            const featureGroup = featureGroupRef.current; // Directly access the FeatureGroup
             if (featureGroup && featureGroup.toGeoJSON) {
                 const drawnItems = featureGroup.toGeoJSON();
-                setGeoJsonData(drawnItems); // Update your state with the new GeoJSON
+                setGeoJsonData(drawnItems);
+            } else {
+                console.error('FeatureGroup does not have toGeoJSON function');
             }
+        } else {
+            console.error('featureGroupRef.current is not set');
         }
     };
-    
+
+
     const onCreate = (e) => {
         const { layerType, layer } = e;
         const featureGroup = e.target; // Get the feature group (contains all layers)'
-        updateGeoJson(); 
+        updateGeoJson();
         updateGeoJson(featureGroup); // Convert to GeoJSON and update state
         updateGeoJson(featureGroupRef.leafletElement); // Convert to GeoJSON and update state
+        console.log("Shape created, GeoJSON updated.", geoJsonData);  // Debugging log
+        console.log("Create event: ", e);  // Debugging log
 
         if (layerType === "marker") {
             const { lat, lng } = layer.getLatLng();
@@ -91,10 +99,53 @@ const MyMap = () => {
 
     useEffect(() => {
         if (featureGroupRef.current) {
+            console.log("FeatureGroupRef: ", featureGroupRef.current);
             // Additional initialization if needed
         }
     }, [featureGroupRef]);
-    
+
+    const downloadGeoJson = (geoJsonData) => {
+        if (geoJsonData) {
+            const blob = new Blob([JSON.stringify(geoJsonData)], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'map-data.geojson';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            console.error("No GeoJSON data to save.");
+            console.log("Current geoJsonData state: ", geoJsonData);
+        }
+    };
+
+
+    const onFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                try {
+                    const text = e.target.result;
+                    const data = JSON.parse(text); // Ensure text is parsed as JSON
+                    if (data) { // Check that data is not null
+                        setGeoJsonData(data);
+                    } else {
+                        console.error("Parsed GeoJSON is null.");
+                    }
+                } catch (error) {
+                    console.error("Error reading GeoJSON: ", error);
+                }
+            };
+            reader.onerror = (error) => console.error('Error reading file: ', error);
+            reader.readAsText(file);
+        } else {
+            console.log("No file selected");
+        }
+    };
+
+
 
     {
         markers.map((position, idx) => (
@@ -114,39 +165,52 @@ const MyMap = () => {
 
 
     return (
+        <div>
+            {/* Load GeoJSON File */}
+            <input type="file" onChange={onFileChange} accept=".geojson,application/json" />
 
-        <MapContainer center={position} zoom={zoom} style={{ height: '100vh', width: '100%' }}>
-            <LayersControl position="topright">
-                <BaseLayer checked name="OpenStreetMap">
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    />
-                    <FeatureGroup ref={(fg) => { if(fg) { setFeatureGroupRef(fg); } }}>
-                        <EditControl
-                            position="topright"
-                            onCreated={onCreate}
-                            onEdited={onEdited}
-                            onDeleted={onDeleted}
-                            draw={{
-                                rectangle: false,  // Disables drawing rectangles
-                                // Customize other draw options as per your requirement
-                            }}
+            {/* Save Button */}
+            <button onClick={() => downloadGeoJson(geoJsonData)}>Save Drawings</button>
+
+            <MapContainer center={position} zoom={zoom} style={{ height: '100vh', width: '100%' }}>
+                <LayersControl position="topright">
+                    {/* Base Layer - OpenStreetMap */}
+                    <BaseLayer checked name="OpenStreetMap">
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         />
-                    </FeatureGroup>
-                    {geoJsonData && <GeoJSON data={geoJsonData} />}
-            {/* ... other base layers ... */}
-                </BaseLayer>
-                <BaseLayer name="Esri WorldImagery">
-                    <TileLayer
-                        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                    </BaseLayer>
+
+                    {/* Base Layer - Esri WorldImagery */}
+                    <BaseLayer name="Esri WorldImagery">
+                        <TileLayer
+                            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                        />
+                    </BaseLayer>
+                </LayersControl>
+
+                {/* FeatureGroup for drawing */}
+                <FeatureGroup ref={featureGroupRef}>
+                    <EditControl
+                        position="topright"
+                        onCreated={onCreate}
+                        onEdited={onEdited}
+                        onDeleted={onDeleted}
+                        draw={{
+                            rectangle: false, // Customize other draw options as per your requirement
+                            // Add or remove drawing options here
+                        }}
                     />
-                </BaseLayer>
-            </LayersControl>
-        </MapContainer>
+                </FeatureGroup>
+
+                {/* GeoJSON to render drawn items */}
+                {geoJsonData && <GeoJSON data={geoJsonData} />}
+
+            </MapContainer>
+        </div>
     );
 };
 
 export default MyMap;
-
