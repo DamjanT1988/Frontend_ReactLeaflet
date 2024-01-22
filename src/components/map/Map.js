6ec92c21b38d99612de4667a79bf6f15020bb313
DamjanT1988 <Damjan_t@hotmail.com>
2024-01-22 13:40:36 +0100

    240122-1400

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -87,7 +87,10 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
   const [geoJsonData, setGeoJsonData] = useState(null);
   const [saveStatus, setSaveStatus] = useState('');
   const accessToken = localStorage.getItem('accessToken'); // Get the access token from local storage
- 
+  //const [cropCoordinates, setCropCoordinates] = useState(null);
+  //const [invertedMaskLayer, setInvertedMaskLayer] = useState(null);
+  //const [rectangleLayer, setRectangleLayer] = useState(null);
+
 
   useEffect(() => {
     if (featureGroupRef.current) {
@@ -106,9 +109,127 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
             }
           }
         });
+
+        featureGroupRef.current.eachLayer(layer => {
+          if (layer.feature && layer.feature.properties.shape === "rectangleCrop") {
+            layer.setStyle({
+              color: 'red',
+              weight: 5,
+              fillOpacity: 0
+            });
+            // Create an inverted mask
+            createInvertedMask(layer);
+          }
+        });
+
       }
+
+      //load  in crop coordinates
+      /*
+      const bounds = [[51.531763, -0.213547], [51.5, -0.06]];
+
+        // Create and style the rectangle
+        const rectangle = L.rectangle(bounds, {
+          color: 'red',
+          weight: 2,
+          fillOpacity: 0.2
+        });
+  
+        // Add the rectangle to the map
+        rectangle.addTo(featureGroupRef.current);
+        */
     }
   }, [geoJsonData]);
+
+
+  const createInvertedMask = (rectangleLayer) => {
+    const bounds = rectangleLayer.getBounds();
+
+    // Large bounds to cover the whole map, adjust as needed
+    const largeBounds = [[90, -180], [-90, 180]];
+
+    // Coordinates for the large polygon covering the entire map
+    const outerCoords = [
+      largeBounds[0], // Top-left of map
+      [90, 180], // Top-right of map
+      largeBounds[1], // Bottom-right of map
+      [-90, -180], // Bottom-left of map
+      largeBounds[0] // Close the loop
+    ];
+
+    // Coordinates for the inner rectangle (smaller rectangle)
+    const innerCoords = [
+      [bounds.getNorthWest().lat, bounds.getNorthWest().lng],
+      [bounds.getNorthEast().lat, bounds.getNorthEast().lng],
+      [bounds.getSouthEast().lat, bounds.getSouthEast().lng],
+      [bounds.getSouthWest().lat, bounds.getSouthWest().lng],
+      [bounds.getNorthWest().lat, bounds.getNorthWest().lng] // Close the loop
+    ];
+
+    // Create a multi-polygon with the outer large rectangle and inner cut-out rectangle
+    const invertedPolygon = L.polygon([outerCoords, innerCoords], {
+      color: 'grey',
+      fillColor: 'black',
+      fillOpacity: 0.3 // Adjust for desired opacity outside the smaller rectangle
+    }).addTo(featureGroupRef.current);
+
+
+    //invertedPolygon.on('click', deleteRectangleAndMask);
+    //setInvertedMaskLayer(invertedPolygon);
+    //setRectangleLayer(layer);
+
+    // Optionally, bring the original rectangle to front
+    rectangleLayer.bringToFront();
+
+    // Add a property to identify the mask
+    invertedPolygon.isMask = true;
+
+    // Store the inverted mask layer reference
+    //setInvertedMaskLayer(invertedPolygon);
+    //return invertedPolygon;
+    console.log('createlayer: ', featureGroupRef.current);
+    console.log('rectanglelayr: ', rectangleLayer);
+  };
+
+  const updateInvertedMask = (rectangleLayer, invertedMask) => {
+    const bounds = rectangleLayer.getBounds();
+    const largeBounds = [[90, -180], [-90, 180]];
+
+    // Update the coordinates of the inverted mask
+    const newOuterCoords = [
+      largeBounds[0], // Top-left of map
+      [90, 180], // Top-right of map
+      largeBounds[1], // Bottom-right of map
+      [-90, -180], // Bottom-left of map
+      largeBounds[0] // Close the loop
+    ];
+    const newInnerCoords = [
+      [bounds.getNorthWest().lat, bounds.getNorthWest().lng],
+      [bounds.getNorthEast().lat, bounds.getNorthEast().lng],
+      [bounds.getSouthEast().lat, bounds.getSouthEast().lng],
+      [bounds.getSouthWest().lat, bounds.getSouthWest().lng],
+      [bounds.getNorthWest().lat, bounds.getNorthWest().lng] // Closing the loop
+    ];
+
+    invertedMask.setLatLngs([newOuterCoords, newInnerCoords]);
+
+
+  };
+
+  /*  const deleteRectangleAndMask = (rectangleLayer, invertedMask) => {
+      featureGroupRef.current.removeLayer(rectangleLayer);
+      featureGroupRef.current.removeLayer(invertedMask);
+    };
+  */
+  /*
+  const deleteRectangleAndMask = () => {
+    if (invertedMaskLayer) {
+      //featureGroupRef.current.removeLayer(rectangleLayer);
+      featureGroupRef.current.removeLayer(invertedMaskLayer);
+      setInvertedMaskLayer(null);
+    }
+  };
+  */
 
   // Function to save GeoJSON data to the server
   const saveDataToServer = async () => {
@@ -136,7 +257,6 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
     }
   };
 
-  // Function to load data from the server
   const loadDataFromServer = async () => {
     try {
       const response = await fetch(`${API_URLS.PROJECT_FILES_GET}/${userID}/${selectedProjectId}/file`, {
@@ -149,6 +269,8 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
         const data = await response.json();
         setGeoJsonData(data);
         console.log('data: ', data);
+
+
       } else {
         console.error('Failed to load data');
       }
@@ -157,16 +279,53 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
     }
   };
 
+
   // useEffect hook to call loadDataFromServer on component mount
   useEffect(() => {
     loadDataFromServer();
+
   }, []);
+
+  /*
+  const redCrop = () => {
+
+      featureGroupRef.current.clearLayers(); // Clear existing layers first
+      L.geoJSON(geoJsonData, {
+        style: (feature) => {
+          if (feature.properties.shape === "rectangle") {
+            return {
+              color: 'red',
+              weight: 2,
+              fillOpacity: 0.2
+            };
+          }
+        }
+      }).addTo(featureGroupRef.current);
+
+      featureGroupRef.current.eachLayer(layer => {
+        if (layer.feature && layer.feature.properties.shape === "rectangle") {
+          layer.setStyle({
+            color: 'red',
+            weight: 2,
+            fillOpacity: 0.2
+          });
+        }
+      });
+
+    
+  };
+  */
+
 
   const updateGeoJson = () => {
     if (featureGroupRef.current) {
       const features = [];
 
       featureGroupRef.current.eachLayer(layer => {
+        // Exclude the mask layer
+        if (layer.isMask) {
+          return;
+        }
         if (layer instanceof L.Circle) {
           const circleFeature = {
             type: 'Feature',
@@ -180,6 +339,31 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
             }
           };
           features.push(circleFeature);
+        } else if (layer instanceof L.Rectangle) {
+
+          // Handle rectangle layer
+          const bounds = layer.getBounds();
+          const rectangleCoordinates = [
+            [bounds.getSouthWest().lng, bounds.getSouthWest().lat],
+            [bounds.getNorthWest().lng, bounds.getNorthWest().lat],
+            [bounds.getNorthEast().lng, bounds.getNorthEast().lat],
+            [bounds.getSouthEast().lng, bounds.getSouthEast().lat],
+            [bounds.getSouthWest().lng, bounds.getSouthWest().lat] // Closing the loop
+          ];
+
+          const rectangleFeature = {
+            type: "Feature",
+            properties: {
+              shape: "rectangleCrop"
+            },
+            geometry: {
+              type: "Polygon",
+              coordinates: [rectangleCoordinates]
+            }
+          };
+          features.push(rectangleFeature);
+
+          //updateInvertedMask(layer, featureGroupRef.current.getLayers()[1]);
         } else {
           // For other shapes, use the default toGeoJSON method
           const layerFeature = layer.toGeoJSON();
@@ -197,18 +381,101 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
     }
   };
 
+
   const onCreate = (e) => {
+    /*   const { layer } = e;
+       if (layer instanceof L.Rectangle) {
+         // Style the rectangle
+         layer.setStyle({
+           color: 'red',        // Color of the rectangle border
+           fillColor: 'red',    // Color of the rectangle fill
+           fillOpacity: 0.2,    // Transparency of the fill
+           weight: 2            // Width of the rectangle border
+         });
+         
+         /*
+         const bounds = layer.getBounds();
+         setCropCoordinates(bounds.toBBoxString()); // or use bounds.getNorthEast(), bounds.getSouthWest()
+         */
+    /*
+    const bounds = layer.getBounds();
+    const rectangleCoordinates = [
+      bounds.getSouthWest().lng, bounds.getSouthWest().lat,
+      bounds.getNorthWest().lng, bounds.getNorthWest().lat,
+      bounds.getNorthEast().lng, bounds.getNorthEast().lat,
+      bounds.getSouthEast().lng, bounds.getSouthEast().lat,
+      bounds.getSouthWest().lng, bounds.getSouthWest().lat // Closing the loop
+    ];
+
+    // Create a new GeoJSON feature
+    const newFeature = {
+      type: "Feature",
+      properties: {
+        shape: "rectangle"
+      },
+      geometry: {
+        type: "Polygon",
+        coordinates: [rectangleCoordinates]
+      }
+    };
+
+    // Update the geoJsonData state
+    setGeoJsonData(prevData => ({
+      ...prevData,
+      features: [...prevData.features, newFeature]
+    }));
+    */
+    // Optionally, send this data to the server
+    // ...
+    //}
+    // Send these coordinates to the server or use as needed
     updateGeoJson(); // Update GeoJSON when new shape is created
   };
 
+
+
   const onEdited = (e) => {
+    e.layers.eachLayer((editedLayer) => {
+      if (editedLayer.feature && editedLayer.feature.properties.shape === "rectangleCrop") {
+        // Remove existing inverted mask
+        let maskToRemove = null;
+        featureGroupRef.current.eachLayer((layer) => {
+          if (layer !== editedLayer && !layer.feature) { // Assuming the mask doesn't have a 'feature' property
+            maskToRemove = layer;
+          }
+        });
+        if (maskToRemove) {
+          featureGroupRef.current.removeLayer(maskToRemove);
+        }
+
+        // Recreate the inverted mask with new coordinates
+        createInvertedMask(editedLayer);
+      }
+    });
+
     updateGeoJson(); // Update GeoJSON when shapes are edited
   };
 
   const onDeleted = (e) => {
+    //deleteRectangleAndMask();
+    // Attach the event handler to the rectangle layer
+    //featureGroupRef.on('click', deleteRectangleAndMask);
+    // Check if the deleted layer is a rectangle with the 'rectangleCrop' property
+    /*
+          if (e.layer.feature && e.layer.feature.properties.shape === "rectangleCrop") {
+      // Find the associated inverted mask and delete it
+      featureGroupRef.current.eachLayer(layer => {
+        if (layer !== e.layer && !layer.feature) { // Assuming the mask doesn't have a 'feature' property
+          featureGroupRef.current.removeLayer(layer);
+        }
+      });
+    }
+*/
+    // Attach a click event to the rectangle layer to delete both
+    //e.layer.on('click', () => deleteRectangleAndMask(e.layer));
+
     updateGeoJson(); // Update GeoJSON when shapes are deleted
   };
-
 
 
   return (
@@ -231,7 +498,16 @@ const Map = ({ selectedProjectId, onSave, userID, /*geoJsonData*/ }) => {
             onCreated={onCreate}
             onEdited={onEdited}
             onDeleted={onDeleted}
-            draw={{ rectangle: false }}
+
+            draw={{
+              rectangle: {
+                shapeOptions: {
+                  color: 'red',
+                  weight: 2,
+                  fillOpacity: 0.2
+                }
+              }
+            }}
           />
         </FeatureGroup>
       </MapContainer>
